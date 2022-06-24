9b64a987eacecd38a0be02e9af006c4b917d4e85
ya-nsh <yansh2017@gmail.com>
2022-06-24 20:42:20 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return <div className="app">Lightning Network</div>;
+}
+
+export default App;
